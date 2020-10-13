e1ee25185b71bba724926bed610637bb9d9a81f1
sakilk130 <sakilk130@gmail.com>
2020-10-13 11:36:25 +0600

    add material-ui & react-icons

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,29 +1,46 @@
 import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
+import { CgPlayListAdd } from 'react-icons/cg';
 
 function Todo() {
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
 
+  //input onChange
   const handleChange = (e) => {
     setInput(e.target.value);
     // console.log(input);
   };
 
+  // button onClick
   const addTodos = (e) => {
     // console.log('object');
     e.preventDefault();
     setTodos([...todos, input]);
     setInput('');
   };
+
   return (
     <div>
       <h1>Todo App🔥</h1>
+
       <form action="">
         <input type="text" value={input} onChange={handleChange} />
-        <button onClick={addTodos} type="submit">
+
+        <Button
+          onClick={addTodos}
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="small"
+        >
+          <CgPlayListAdd /> Add
+        </Button>
+        {/* <button onClick={addTodos} type="submit">
           +Add
-        </button>
+        </button> */}
       </form>
+
       <ul>
         {todos.map((todo) => (
           <li key={todo}>{todo}</li>
